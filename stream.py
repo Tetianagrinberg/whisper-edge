a72553396c1079e86e375be9974e6a016591c3b4
Tetianagrinberg <87442740+Tetianagrinberg@users.noreply.github.com>
2023-10-26 16:15:54 -0700

    Update stream.py: remove blocking on queue put and get, 5 sec chunks

--- stream.py
+++ stream.py
@@ -22,7 +22,7 @@ flags.DEFINE_integer('num_channels', 1,
                      'The number of channels of the recorded audio.')
 flags.DEFINE_integer('channel_index', 0,
                      'The index of the channel to use for transcription.')
-flags.DEFINE_integer('chunk_seconds', 10,
+flags.DEFINE_integer('chunk_seconds', 5,
                      'The length in seconds of each recorded chunk of audio.')
 flags.DEFINE_string('latency', 'low', 'The latency of the recording stream.')
 
@@ -57,18 +57,21 @@ def stream_callback(indata, frames, time, status, audio_queue):
 
     # Add this chunk of audio to the queue.
     audio = indata[:, FLAGS.channel_index].copy()
-    audio_queue.put(audio)
+    audio_queue.put_nowait(audio)
 
 
 @timed
 def process_audio(audio_queue, model):
     # Block until the next chunk of audio is available on the queue.
-    start_q = now() 
-    audio = audio_queue.get()
-    stop_q = now()
-    print(f"pulling from queue took took {stop_q-start_q:.3f}s")
-    # Transcribe the latest audio chunk.
-    transcribe(model=model, audio=audio)
+    if not audio_queue.empty():
+      start_q = now() 
+      audio = audio_queue.get_nowait()
+      stop_q = now()
+      print(f"pulling from queue took took {stop_q-start_q:.3f}s")
+      # Transcribe the latest audio chunk.
+      transcribe(model=model, audio=audio)
+    else:
+      pass
 
 
 def main(argv):

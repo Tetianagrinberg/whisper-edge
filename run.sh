3bf1b5d02aa7e2fe9c1a9ebd23f18ddd9940baf6
Tetianagrinberg <87442740+Tetianagrinberg@users.noreply.github.com>
2023-10-27 14:15:55 -0700

    Update run.sh remove space in line 13-14

--- run.sh
+++ run.sh
@@ -10,8 +10,8 @@ sudo docker run \
   --rm  \
   --network host \
   --device /dev/snd \
-  --mount type=bind, source=/sys/bus/i2c/drivers/ina3221x/6-0040/iio:device0/, destination=/jetson-inference/power/ \
-  --mount type=bind, source=/home/bongard/Desktop/speeches/, destination=/jetson-inference/speeches/\
+  --mount type=bind,source=/sys/bus/i2c/drivers/ina3221x/6-0040/iio:device0/,destination=/jetson-inference/power/ \
+  --mount type=bind,source=/home/bongard/Desktop/speeches/,destination=/jetson-inference/speeches/ \
   whisper-inference \
   python power_inference.py \
   $@

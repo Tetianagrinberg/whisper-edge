409385ec79709745c484fdc5dafe01eabd8abdd9
Tetianagrinberg <87442740+Tetianagrinberg@users.noreply.github.com>
2023-10-27 12:36:05 -0700

    Update power_inference.py
    - Add correct path for speeches
    - Add print statements in main
    - change np array initialized size

--- power_inference.py
+++ power_inference.py
@@ -6,7 +6,7 @@ import threading
 import os
 import torch
 
-def record_power_consumption(arr, interval=0.1, duration=10):
+def record_power_consumption(arr, interval=0.1, duration=30):
     start_time = time.time()
     index = 0
     while time.time() - start_time < duration:
@@ -24,12 +24,11 @@ def record_power_consumption(arr, interval=0.1, duration=10):
 
 
 def save_csv(np_array, file_name):
-
     df = pd.DataFrame(np_array)
     df.to_csv(file_name)
 
 def run_whisper_inference(latencies, whisper_model):
-    rec_folder = "/speeches/"
+    rec_folder = "/home/bongard/Desktop/speeches/" #replace directory if needed
     speech_index = 0
     for audio in os.listdir(rec_folder):
         inference_start = time.time()
@@ -44,26 +43,32 @@ def run_whisper_inference(latencies, whisper_model):
 # Main script
 if __name__ == "__main__":
     # Define the array size: duration / interval
+    print("Loading model...")
     model = whisper.load_model(name='tiny.en')
+    
+    print("Initializing model buffer...")
     dummy_recording_size = 30 * 16000 #seconds times sample rate
     whisper.transcribe(model=model,
                        audio=np.zeros(dummy_recording_size, dtype=np.float32))
 
-    array_size = int(10 / 0.1)
+    array_size = int(1000 / 0.1)
     power_readings = np.zeros(array_size)
-    latencies = np.array(100)
-
+    latencies = np.zeros(int(1000))
 
     thread_power = threading.Thread(target=record_power_consumption, args=(power_readings,))
     thread_inference = threading.Thread(target=run_whisper_inference, args=(latencies, model))
 
     # Start the threads
-    thread_power.start()
+    print("starting inference thread...")
     thread_inference.start()
+    print("starting power thread...")
+    thread_power.start()
 
     # Wait for the thread to complete
     thread_power.join()
     thread_inference.join()
+
+    print("all threads complete")
 
     #remove unfilled cells in  both arrays
     np.trim_zeros(power_readings)

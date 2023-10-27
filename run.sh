6c27cc1cddb67521a2c48051bbb4c530fdd7bd11
Tetianagrinberg <87442740+Tetianagrinberg@users.noreply.github.com>
2023-10-27 13:47:01 -0700

    Update run.sh: mount speeches and power sensor

--- run.sh
+++ run.sh
@@ -10,6 +10,8 @@ sudo docker run \
   --rm  \
   --network host \
   --device /dev/snd \
+  --mount type=bind, source=/sys/bus/i2c/drivers/ina3221x/6-0040/iio:device0/, destination=/var/power/ \
+  --mount type=bind, source=/home/bongard/Desktop/speeches/, destination=/var/speeches/\
   whisper-inference \
   python power_inference.py \
   $@

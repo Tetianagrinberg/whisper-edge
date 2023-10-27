6716fac37772604421a44e623fc3a4477ba35cb9
Tetianagrinberg <87442740+Tetianagrinberg@users.noreply.github.com>
2023-10-27 16:29:44 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ sudo docker run \
   --network host \
   --device /dev/snd \
   --mount type=bind,source=/sys/bus/i2c/drivers/ina3221x/6-0040/iio:device0/,destination=/jetson-inference/power/ \
-  --mount type=bind,source=/home/bongard/Desktop/outputs/,destination=/jetson-inference/speeches/
+  --mount type=bind,source=/home/bongard/Desktop/outputs/,destination=/jetson-inference/speeches/ \
   whisper-inference \
   python run_inference.py \
   $@

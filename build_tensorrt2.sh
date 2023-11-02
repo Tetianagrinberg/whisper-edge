c7b392c8d773bad9fa8472c0579f2020b26f43bc
Tetianagrinberg <87442740+Tetianagrinberg@users.noreply.github.com>
2023-11-01 21:08:17 -0700

    Create build_tensorrt2.sh

---
+++ build_tensorrt2.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Exit on error.
+set -e
+
+# Make the GPU available during the build.
+sudo bash -c "cat > /etc/docker/daemon.json" << EOL
+{
+    "default-runtime": "nvidia",
+    "runtimes": {
+        "nvidia": {
+            "path": "nvidia-container-runtime",
+            "runtimeArgs": []
+        }
+    }
+}
+EOL
+sudo systemctl restart docker
+
+# Build the docker image.
+sudo docker build \
+  -f whisper-edge/Dockerfile.tensorrt \
+  -t tensorrt \
+  whisper-edge/
